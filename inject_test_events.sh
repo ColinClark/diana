0ca9982fb799dbed1591bdff691f341b7b0aa395
Colin Clark <colin.clark+statista@statista.com>
2025-06-12 08:07:22 +0200

    removed unneccesary files

--- inject_test_events.sh
+++
@@ -1,8 +0,0 @@
-# in the project directory
-docker compose exec -T broker  \
-  kafka-topics --bootstrap-server localhost:9092 --create \
-    --replication-factor 1 --partitions 1 --topic web-events2 2>/dev/null || true
-
-cat events.ndjson | docker compose exec -T broker  \
-  kafka-console-producer --bootstrap-server localhost:9092 --topic web-events2 --property "parse.key=false"
-
